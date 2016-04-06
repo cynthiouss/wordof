3f7638b60cc5bc56b3432429ebc1a1adfe7eb399
Adrien Dubois <adrien.dubois.be@gmail.com>
2016-04-06 19:34:22 +0200

    Improvements done

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,9 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+// Slow Scroll
 $('a').click(function(){
     $('html, body').animate({
         scrollTop: $( $.attr(this, 'href') ).offset().top
     }, 1500);
     return false;
+});
+
+
+$(document).ready(function() {
+   $('.selectpicker').selectpicker();
 });
