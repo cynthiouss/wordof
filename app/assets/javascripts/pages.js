c2837a38a5cb365215826880863a49f0745fea44
Adrien Dubois <adrien.dubois.be@gmail.com>
2016-04-07 19:18:05 +0200

    merged Nathaniels code but still have issues with Javascript

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -20,3 +20,34 @@ $('#emailmodal').on('show.bs.modal', function (event) {
   modal.find('.modal-title').text('New message to ' + recipient)
   modal.find('.modal-body input').val(recipient)
 })
+
+// Cards and Caroussel
+$(function() {
+  $('.material-card > .mc-btn-action').click(function () {
+      var card = $(this).parent('.material-card');
+      var icon = $(this).children('i');
+      icon.addClass('fa-spin-fast');
+
+      if (card.hasClass('mc-active')) {
+          card.removeClass('mc-active');
+
+          window.setTimeout(function() {
+              icon
+                  .removeClass('fa-arrow-left')
+                  .removeClass('fa-spin-fast')
+                  .addClass('fa-bars');
+
+          }, 800);
+      } else {
+          card.addClass('mc-active');
+
+          window.setTimeout(function() {
+              icon
+                  .removeClass('fa-bars')
+                  .removeClass('fa-spin-fast')
+                  .addClass('fa-arrow-left');
+
+          }, 800);
+      }
+  });
+});
