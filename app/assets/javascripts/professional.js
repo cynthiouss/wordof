6cb5962d64db6fba505b140398720942228f19c9
Cynthia Sarafianou <cynthiasarafianou@gmail.com>
2016-04-08 18:33:39 +0200

    linking bend and fend

--- app/assets/javascripts/professional.js
+++ app/assets/javascripts/professional.js
@@ -1,2 +1,53 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+// Slow Scroll
+$('.slow-scroll').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 1500);
+    return false;
+});
+
+
+// Mail modal
+$('#emailmodal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var recipient = button.data('whatever') // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  modal.find('.modal-title').text('New message to ' + recipient)
+  modal.find('.modal-body input').val(recipient)
+})
+
+// Cards and Caroussel
+$(function() {
+  $('.material-card > .mc-btn-action').click(function () {
+      var card = $(this).parent('.material-card');
+      var icon = $(this).children('i');
+      icon.addClass('fa-spin-fast');
+
+      if (card.hasClass('mc-active')) {
+          card.removeClass('mc-active');
+
+          window.setTimeout(function() {
+              icon
+                  .removeClass('fa-arrow-left')
+                  .removeClass('fa-spin-fast')
+                  .addClass('fa-bars');
+
+          }, 400);
+      } else {
+          card.addClass('mc-active');
+
+          window.setTimeout(function() {
+              icon
+                  .removeClass('fa-bars')
+                  .removeClass('fa-spin-fast')
+                  .addClass('fa-arrow-left');
+
+          }, 400);
+      }
+  });
+});
